ecbc8861fcd0c80f04b9eafb737b669ff07a3320
Anisha Tamne <anishatamne@gmail.com>
2025-02-03 19:40:56 +0530

    User Input Updated

--- UserInput.java
+++ UserInput.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class UserInput {
-    private Scanner scanner;
+    private Scanner scanner; // Scanner object for user input
 
+ // Constructor to initialize the Scanner object
     public UserInput() {
         scanner = new Scanner(System.in);
     }
 
-    
+    //Method to get an array of integers from the user.
     public int[] getNumbers() {
         System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        
+        int n = scanner.nextInt();   // Read the number of elements
+        int[] numbers = new int[n];   // Create an array of size n
+         
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
+            numbers[i] = scanner.nextInt();     // Read each number and store in the array
         }
-        return numbers;
-    }
+        return numbers;     // Return the filled array
+
+    }
 }

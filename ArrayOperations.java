084e730a2852169bd31072bb3be4f79afe9c8ec9
Anisha Tamne <anishatamne@gmail.com>
2025-02-03 19:19:55 +0530

    Array Operations 2nd commit

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -13,3 +13,20 @@ public class ArrayOperations {
                 oddList.add(num);
             }
         }
+
+    
+// Convert the ArrayLists to arrays
+int[] even = new int[evenList.size()];
+int[] odd = new int[oddList.size()];
+
+for (int i = 0; i < evenList.size(); i++) {
+    even[i] = evenList.get(i);
+}
+for (int i = 0; i < oddList.size(); i++) {
+    odd[i] = oddList.get(i);
+}
+
+System.out.println("Even numbers: " + Arrays.toString(even));
+System.out.println("Odd numbers: " + Arrays.toString(odd));
+}
+

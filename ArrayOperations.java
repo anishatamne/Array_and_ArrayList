3d94a5b08e2b57eca7505c4cfc5d7289498c7d4f
Anisha Tamne <anishatamne@gmail.com>
2025-02-03 19:26:37 +0530

    array list to array

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -58,3 +58,13 @@ public static ArrayList<Integer> arrayToArrayList(int[] array) {
     }
     return list;
 }
+
+
+public static int[] arrayListToArray(ArrayList<Integer> list) {
+    int[] array = new int[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+        array[i] = list.get(i);
+    }
+    return array;
+    }
+}
